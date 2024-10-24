e81500e1c262d5ddfc92f4eb07e672ab9a50b596
osama adel <osama.adel7358@gmail.com>
2024-10-24 11:18:15 +0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-export function hello() {
-  return "hello";
-}
+document.body.textContent = 123;
--- script.test.js
+++ script.test.js
@@ -1,14 +1,14 @@
-import { hello } from "./script";
+'use strict';
 
-describe("label", () => {
-  it("label", () => {
-    expect(hello()).toMatchSnapshot();
-    expect(hello()).toBe("hello");
-    expect({k:"v"}).toEqual({k:"v"});
-    expect(5).toBeGreaterThan(4);
-    expect(5).toBeGreaterThanOrEqual(5);
-    expect(4).toBeLessThan(5);
-    expect(5).toBeLessThanOrEqual(5);
-  });
+const { JSDOM } = require('jsdom');
 
+const options = {
+  resources: 'usable',
+  runScripts: 'dangerously',
+};
+
+JSDOM.fromFile('index.html', options).then((dom) => {
+  dom.window.document.addEventListener('DOMContentLoaded', () => {
+    console.log(dom.window.document.body.textContent.trim());
+  });  
 });
