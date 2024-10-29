9b89c76ebf7c661911a80007817251467f792ffa
osama adel <osama.adel7358@gmail.com>
2024-10-29 08:42:27 +0300

    ideal

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function incr() {
-  document.getElementById("out").innerHTML++;
-}
+document.getElementById("row1").addEventListener("mouseover", function () {
+  this.style.animationPlayState = "paused"; // Pauses scrolling on hover
+});
+document.getElementById("row1").addEventListener("mouseout", function () {
+  this.style.animationPlayState = "running"; // Resumes scrolling when not hovered
+});
+document.getElementById("row2").addEventListener("mouseover", function () {
+  this.style.animationPlayState = "paused"; // Pauses scrolling on hover
+});
+document.getElementById("row2").addEventListener("mouseout", function () {
+  this.style.animationPlayState = "running"; // Resumes scrolling when not hovered
+});
--- test.js
+++ test.js
@@ -2,22 +2,32 @@
 
 const { JSDOM } = require("jsdom");
 
+const options = {
+  resources: "usable",
+  runScripts: "dangerously",
+};
+
 let /** @type {Window} */ window, /** @type {Document} */ document;
-/** @type {HTMLElement} */
-let /** @type {HTMLElement} */ button, /** @type {HTMLElement} */ out;
+let /** @type {HTMLElement} */ row1, /** @type {HTMLElement} */ row2;
 
 function set() {
-  button = document.getElementById("button");
-  out = document.getElementById("out");
+  // Set all the HTML components that will be interacted with here
+  row1 = document.getElementById("row1");
+  row2 = document.getElementById("row2");
 }
+
 function reset(done) {
-  JSDOM.fromFile("index.html", {
-    resources: "usable",
-    runScripts: "dangerously",
-  }).then((dom) => {
+  JSDOM.fromFile("index.html", options).then((dom) => {
     window = dom.window;
     document = window.document;
-
+    Object.defineProperty(window.HTMLElement.prototype, "innerText", {
+      get() {
+        return this.textContent;
+      },
+      set(value) {
+        this.textContent = value;
+      },
+    });
     if (document.readyState != "loading") {
       set();
       done();
@@ -29,26 +39,41 @@ function reset(done) {
   });
 }
 
-describe("Group 1", () => {
-  beforeAll((done) => {
-    reset(done);
-  });
-  it("Test 1", () => {
-    console.log(out.innerHTML);
-    expect(out.innerHTML).toBe("0");
-  });
 
-  it("Test 2", () => {
-    button.click();
-    expect(out.innerHTML).toBe("1");
-  });
+beforeAll(() => {
+  jest.useFakeTimers();
 });
 
-describe("Group 2", () => {
+describe("Carousel Row Functionality", () => {
   beforeAll((done) => {
     reset(done);
   });
-  it("Test 1", () => {
-    expect(out.innerHTML).toBe("0");
+
+  it("Initial animation state of row1 should be running", () => {
+    expect(row1.style.animationPlayState).toBe("");
+  });
+
+  it("Initial animation state of row2 should be running", () => {
+    expect(row2.style.animationPlayState).toBe("");
+  });
+
+  it("Row1 pauses animation on hover", () => {
+    row1.dispatchEvent(new window.MouseEvent("mouseover"));
+    expect(row1.style.animationPlayState).toBe("paused");
+  });
+
+  it("Row1 resumes animation on mouse out", () => {
+    row1.dispatchEvent(new window.MouseEvent("mouseout"));
+    expect(row1.style.animationPlayState).toBe("running");
+  });
+
+  it("Row2 pauses animation on hover", () => {
+    row2.dispatchEvent(new window.MouseEvent("mouseover"));
+    expect(row2.style.animationPlayState).toBe("paused");
+  });
+
+  it("Row2 resumes animation on mouse out", () => {
+    row2.dispatchEvent(new window.MouseEvent("mouseout"));
+    expect(row2.style.animationPlayState).toBe("running");
   });
 });
