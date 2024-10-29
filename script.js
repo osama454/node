d0628b082aa18e5f26a4f39f92568d081c34f692
osama adel <osama.adel7358@gmail.com>
2024-10-29 18:18:44 +0300

    ideal

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function incr() {
-  document.getElementById("out").innerHTML++;
-}
+const promoAd = document.querySelector('.promo-ad');
+const signupBtn = document.querySelector('.signup-btn');
+
+promoAd.addEventListener('mouseover', () => {
+    signupBtn.style.display = 'block';
+});
+
+promoAd.addEventListener('mouseout', () => {
+    signupBtn.style.display = 'none';
+});
